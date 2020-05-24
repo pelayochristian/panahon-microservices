8e615e4832bae4931d47f6fe8baed8309273569b
Christian <pelayochristian.dev@gmail.com>
2020-05-24 20:28:01 +0800

    API Gateway
    Add to the project API Gateway using the Spring Cloud Gateway routing

---
+++ gatewayserver/src/main/java/com/project/panahon/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.project.panahon.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+}
